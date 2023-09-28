fcc62ed9e63623f3b7b065d2a55ccf6f5a181328
NoahM24 <142345250+NoahM24@users.noreply.github.com>
2023-09-28 13:58:26 -0400

    v0.1a
    Declared and defined three lists.

--- gameProgramming/computer_science_exercises/02_Collections/collections.py
+++ gameProgramming/computer_science_exercises/02_Collections/collections.py
@@ -1,1 +1,12 @@
-# Collections Examples, Noah Mulder, v0.0
+# Collections Examples, Noah Mulder, v0.1a
+
+# LIST -- ORDERED, CHANGEABLE, ALLOWS US TO DUPLICATE VALUES
+breakfast_foods = ["Bacon", "Waffles", "Pancakes", "Cereal", "Milk"]
+# Each item on the list is known as an ELEMENT
+# The position in the list for each is the INDEX
+# The element "Bacon" is at index 0
+# Python Only: index -1 it is the last item on the list
+test_scores = [95, 100, 25, 15, 27, 35]
+class_gpa = [3.14, 2.25, 1.74, 1.99, 0.99, 4.25]
+
+
