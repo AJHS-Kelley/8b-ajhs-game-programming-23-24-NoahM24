7568517e29946783aa3752c3aca1f8f45584e663
NoahM24 <142345250+NoahM24@users.noreply.github.com>
2023-10-06 19:41:06 -0400

    collection_game v0.2
    I got the code to work but I think I can do it better. I also added the second part of the project

--- gameProgramming/computer_science_exercises/02_Collections/collection_game.py
+++ gameProgramming/computer_science_exercises/02_Collections/collection_game.py
@@ -1,4 +1,4 @@
-# collection_game v0.1
+# collection_game v0.2
 # Create a list variable named playerInventory.
 # Use a while loop to allow the user to enter 10 items into the player inventory. 
 # Sort the inventory.
@@ -6,16 +6,28 @@
 
 player_inventory = []
 item_chosen = ""
-items_chosen = -1
+items_chosen = 0
 items_left = 10
 
-while player_inventory != 10:
+while items_chosen != 10:
     print(f"You have {items_left - items_chosen} inventory space left.\n")
-    item_chosen = input("What items would you like to add?")
-    print(f"You have chosen {item_chosen} to go into your inventory\n")
+    item_chosen = input("What item would you like to add?\n")
+    print(f"You have chosen {item_chosen} to go into your inventory.\n")
     player_inventory.append(item_chosen)
-    print(player_inventory)
+    items_chosen += 1
+    #print(player_inventory)
     
     
-    #player_inventory.sort()
-print(f"You have {player_inventory} in iventory.")
+player_inventory.sort()
+print(f"You have {player_inventory} in your iventory.")
+
+#while player_inventory >= 5:
+    #print(f"You have {player_inventory} in your iventory.")
+    #item_chosen = input("What item would you like to remove?\n")
+    #print(f"You have chosen {item_chosen} to be removed from your inventory.\n")
+    #player_inventory.remove(item_chosen)
+    #print(player_inventory)
+    
+    
+#player_inventory.sort()
+#print(f"You have {player_inventory} in your iventory.")
