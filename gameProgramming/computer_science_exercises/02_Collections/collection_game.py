5f3491e3ab23b2dddfbc4e783131910081a84570
NoahM24 <142345250+NoahM24@users.noreply.github.com>
2023-10-17 14:09:05 -0400

    collection_game v0.4
    Fixed some things and removed some things.

--- gameProgramming/computer_science_exercises/02_Collections/collection_game.py
+++ gameProgramming/computer_science_exercises/02_Collections/collection_game.py
@@ -1,4 +1,4 @@
-# collection_game v0.3
+# collection_game v0.4
 # Create a list variable named playerInventory.
 # Use a while loop to allow the user to enter 10 items into the player inventory. 
 # Sort the inventory.
@@ -6,28 +6,24 @@
 
 #player_inventory = []
 #item_chosen = ""
-#items_chosen = 0
-#items_left = 10
+weapon_list = []
 
-#while items_chosen != 10:
-    #print(f"You have {items_left - items_chosen} inventory space left.\n")
+#while len(player_inventory) < 10:
     #item_chosen = input("What item would you like to add?\n")
     #print(f"You have chosen {item_chosen} to go into your inventory.\n")
     #player_inventory.append(item_chosen)
-    #items_chosen += 1
     #print(player_inventory)
     
     
 #player_inventory.sort()
 #print(f"You have {player_inventory} in your iventory.")
 
-#while items_chosen > 5:
+#while len(player_inventory) > 5:
     #print(f"You have {player_inventory} in your iventory.")
     #item_chosen = input("What item would you like to remove?\n")
     #print(f"You have chosen {item_chosen} to be removed from your inventory.\n")
     #player_inventory.remove(item_chosen)
     #print(player_inventory)
-    #items_chosen -= 1
     
     
 #player_inventory.sort()
