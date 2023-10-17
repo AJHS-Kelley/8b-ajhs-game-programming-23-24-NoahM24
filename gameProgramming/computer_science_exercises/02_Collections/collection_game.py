5b9c20fca5a2194255f5b43249c017eb332730c7
NoahM24 <142345250+NoahM24@users.noreply.github.com>
2023-10-17 14:49:41 -0400

    collection_game v0.6
    Added the weapon list

--- gameProgramming/computer_science_exercises/02_Collections/collection_game.py
+++ gameProgramming/computer_science_exercises/02_Collections/collection_game.py
@@ -1,4 +1,4 @@
-# collection_game v0.5
+# collection_game v0.6
 # Create a list variable named playerInventory.
 # Use a while loop to allow the user to enter 10 items into the player inventory. 
 # Sort the inventory.
@@ -29,12 +29,12 @@
 #print(f"You have {player_inventory} in your iventory.")
 
 #door_keys = [
-    "red"
-    "green"
-    "yellow"
-    "gold"
-    "blue"
-]
+#    "red"
+#   "green"
+#    "yellow"
+#    "gold"
+#    "blue"
+#]
 
 #key = input("Which color key do you need to unlock the door?\n")
 
@@ -42,3 +42,31 @@
     #print("You have the correct key! The door unlocks.\n")
 #else:
     #print("You do not have that key. The door remains locked.\n")
+    
+#weapon_list = [
+    #True, # Battleaxe
+    #False, # Greatsword
+    #False, # 9mm gun
+    #True, # Long Sword
+    #True, # Gamma Gun
+    #True, # Grapple Gun
+    #False # Frying Pan
+#]
+
+#weapon_num = 0
+#while weapon_num < len(weapon_list):
+    #if weapon_num == 0 and weapon_list[0] == True:
+    #    print("You are wielding a giant Battleaxe!")
+    #if weapon_num == 1 and weapon_list[1] == True:
+    #    print("You are wielding a shiny Greatsword!")
+    #if weapon_num == 2 and weapon_list[2] == True:
+    #    print("You are wielding a small 9mm gun!")
+    #if weapon_num == 3 and weapon_list[3] == True:
+    #    print("You are wielding a sharp Long Sword!")
+    #if weapon_num == 4 and weapon_list[4] == True:
+    #    print("You are wielding a strong Gamma Gun!")
+    #if weapon_num == 5 and weapon_list[5] == True:
+    #    print("You are wielding a useful Grapple Gun!")
+    #if weapon_num == 6 and weapon_list[6] == True:
+    #    print("You are wielding a blunt Frying Pan!")
+    #weapon_num += 1
