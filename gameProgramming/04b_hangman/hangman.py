1c597b2f91af4864c63bd3a52a150ff301bf0a75
NoahM24 <142345250+NoahM24@users.noreply.github.com>
2023-10-31 14:34:53 -0400

    hangman game v0.3
    Added the random word chooser. Printing random words works

--- gameProgramming/04b_hangman/hangman.py
+++ gameProgramming/04b_hangman/hangman.py
@@ -1,5 +1,5 @@
-# Hangman Game ny Noah Mulder, v0.2
-
+# Hangman Game ny Noah Mulder, v0.3
+import random
 words = 'Hangman Silver Gold Diamond Platinum Key Keyboard Gorilla Game Program Pow Apple Banana Chloroplast Job Ask Mitochondria Acid Poison Name Help Suffer Watch Lob Lose Corn Cob Same Nine One'.split()
 
 # VARIABLE_NAME in ALL-CAPS ARE CONSTANTS AND NOT MEANT TO CHANGE!
@@ -40,3 +40,14 @@ HANGMAN_BOARD = ['''
    / \  |       
     ========''']
 
+# Pick Word from List
+def get_randomword(word_list): # Return a random word from the list.
+    word_index = random.randint(0, len(word_list) - 1)
+    # len(list_name) - 1 is EXTREMELY COMMON FOR WORKING WITH LISTS.
+    return word_list[word_index]
+
+i = 0
+while i < 50:
+    word = get_randomword(words)
+    print(word)
+    i += 1
