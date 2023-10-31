59e8740e11752a4a479e9d7dd2947e7b96f86d38
NoahM24 <142345250+NoahM24@users.noreply.github.com>
2023-10-31 14:50:13 -0400

    hangman game v0.4
    Added some more stuff to complete the game

--- gameProgramming/04b_hangman/hangman.py
+++ gameProgramming/04b_hangman/hangman.py
@@ -1,4 +1,4 @@
-# Hangman Game ny Noah Mulder, v0.3
+# Hangman Game ny Noah Mulder, v0.4
 import random
 words = 'Hangman Silver Gold Diamond Platinum Key Keyboard Gorilla Game Program Pow Apple Banana Chloroplast Job Ask Mitochondria Acid Poison Name Help Suffer Watch Lob Lose Corn Cob Same Nine One'.split()
 
@@ -46,8 +46,18 @@ def get_randomword(word_list): # Return a random word from the list.
     # len(list_name) - 1 is EXTREMELY COMMON FOR WORKING WITH LISTS.
     return word_list[word_index]
 
-i = 0
-while i < 50:
-    word = get_randomword(words)
-    print(word)
-    i += 1
+def display_board(missed_letters, correct_letters, secret_word):
+    print(HANGMAN_BOARD[len(missed_letters)])
+    print()
+
+    print('Missed Letters:', end = ' ')
+    for each_letter in missed_letters:
+        print(each_letter, end = ' ')
+    print()
+    # FINISH THURSDAY
+
+# i = 0
+# while i < 50:
+#     word = get_randomword(words)
+#     print(word)
+#     i += 1
