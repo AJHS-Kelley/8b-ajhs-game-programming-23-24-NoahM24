1ea0207d4d10ba311b1321d48bd7125cda75dbba
NoahM24 <142345250+NoahM24@users.noreply.github.com>
2023-10-31 13:58:11 -0400

    hangman game v0.1
    added the list and .split()

--- gameProgramming/04b_hangman/hangman.py
+++ gameProgramming/04b_hangman/hangman.py
@@ -1,1 +1,4 @@
-# Hangman Game ny Noah Mulder, v0.0
+# Hangman Game ny Noah Mulder, v0.1
+
+words = 'Hangman Silver Gold Diamond Platinum Key Keyboard Gorilla Game Program Pow Apple Banana Chloroplast Job Ask Mitochondria Acid Poison Name Help Suffer Watch Lob Lose Corn Cob Same Nine One'.split()
+print(words)
