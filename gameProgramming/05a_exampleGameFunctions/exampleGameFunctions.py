468edfd042fb261c7f1ea80308843e05a5df0b80
NoahM24 <142345250+NoahM24@users.noreply.github.com>
2023-11-13 13:51:17 -0500

    exampleGameFunction.py v1
    something happened

--- gameProgramming/05a_exampleGameFunctions/exampleGameFunctions.py
+++ gameProgramming/05a_exampleGameFunctions/exampleGameFunctions.py
@@ -1,1 +1,14 @@
-# Example Game Functions Project, Noah Mulder, v0
+# Example Game Functions Project, Noah Mulder, v1
+import random
+
+def function_one():
+    pass
+
+def function_two(param1):
+    pass
+
+def function_three(param1 = "Default Value"):
+    pass
+
+def function_four(param1, param2):
+    pass
