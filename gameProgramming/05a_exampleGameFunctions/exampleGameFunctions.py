8c313981b55ee7d8b7c2a63d45d04551bacc6fe0
NoahM24 <142345250+NoahM24@users.noreply.github.com>
2023-11-13 14:07:02 -0500

    exampleGameFunctions.py v2
    Got a function but it don't print

--- gameProgramming/05a_exampleGameFunctions/exampleGameFunctions.py
+++ gameProgramming/05a_exampleGameFunctions/exampleGameFunctions.py
@@ -1,11 +1,20 @@
-# Example Game Functions Project, Noah Mulder, v1
+# Example Game Functions Project, Noah Mulder, v2
 import random
+
+player_hp = 100
+cpu_hp = 100
+player_name = ""
+cpu_name = ""
+attack_potency = 0.0
+elemental_attack = False
+cpu_namelist = ["Rodrick", "Grag", "Tyu", "Boder", "Poly"]
 
 def function_one():
     pass
 
-def function_two(param1):
-    pass
+def get_cpuname(cpu_namelist):
+    name_index = random.randint(0, len(cpu_namelist) - 1)
+    print(name_index)
 
 def function_three(param1 = "Default Value"):
     pass
