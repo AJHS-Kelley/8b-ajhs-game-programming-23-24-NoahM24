0ade9d2ccb2c0b4666cee6f240c175fa2bf66d57
NoahM24 <142345250+NoahM24@users.noreply.github.com>
2023-11-28 13:41:59 -0500

    exampleGameFunction.py v5
    Fixed some things

--- gameProgramming/05a_exampleGameFunctions/exampleGameFunctions.py
+++ gameProgramming/05a_exampleGameFunctions/exampleGameFunctions.py
@@ -1,4 +1,4 @@
-# Example Game Functions Project, Noah Mulder, v4
+# Example Game Functions Project, Noah Mulder, v5
 import random
 
 player_hp = 100
@@ -11,9 +11,6 @@ cpu_namelist = ["Rodrick", "Grag", "Tyu", "Boder", "Poly"]
 attack_type = ""
 
 player_name = input("What is your characters name?\n")
-
-print(f"{player_name} HP: {player_hp}\n")
-print(f"{cpu_name} HP: {cpu_hp}\n")
 
 def function_one():
     pass
@@ -30,14 +27,18 @@ def function_four(param1, param2):
     pass
 
 while player_hp or cpu_hp != 0:
+    print(f"{player_name} HP: {player_hp}\n")
+    print(f"{cpu_name} HP: {cpu_hp}\n")
     attack_type = input("What type of attack would you like to use? Light or Heavy?\n")
     if "light" or "Light":
-        attack_potency = random.randint(0.0, 5.0)
-        health_taken = 5 * {attack_potency}
+        attack_potency = random.randint(1.0, 5.0)
+        print(attack_potency)
+        health_taken = 5 * (attack_potency)
         cpu_hp -= health_taken
     elif "heavy" or "Heavy":
-        attack_potency = random.randint(0.0, 5.0)
-        health_taken = 10 * {attack_potency}
+        attack_potency = random.randint(1.0, 5.0)
+        print(attack_potency)
+        health_taken = 10 * (attack_potency)
         cpu_hp -= health_taken
     else:
         print("Choose of the two attack types.")
