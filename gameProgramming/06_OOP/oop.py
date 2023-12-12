5497e73f90c66b93e6f1f907680482126b8b7fc5
NoahM24 <142345250+NoahM24@users.noreply.github.com>
2023-12-12 14:26:57 -0500

    oop.py v1.1
    something

--- gameProgramming/06_OOP/oop.py
+++ gameProgramming/06_OOP/oop.py
@@ -1,4 +1,4 @@
-# Object-Oriented Programming, Noah Mulder, v1.0
+# Object-Oriented Programming, Noah Mulder, v1.1
 
 class Person: # Yse PascalCase for ClassNames
     def __init__(self, name, age, weight):
@@ -18,21 +18,47 @@ class Person: # Yse PascalCase for ClassNames
 
 person1 = Person("Terry Bogard", 32, 156)
 person2 = Person("Mario", 59, 112)
-print(person1)
+# print(person1)
+# print(person2)
+
+# if person1.weight > person2.weight:
+#    print(f"{person1.name} weighs more than {person2.name}.")
+# elif person1.weight == person2.weight:
+#    print("Each person weighs the same.\n")
+# else:
+#    print(f"{person2.name} weighs more than {person1.name}.")
+
+# if person1.age > person2.age:
+#    print(f"{person1.name} is older than {person2.name}.")
+# elif person1.age == person2.age:
+#    print(f"{person1.name} is the same age as {person2.name}")
+# else:
+#    print(f"{person2.name} is older than {person1.name}")
+
+# person1.classfunction()
+
+# Changing Properties After Creation
+print(person1.name)
+person1.name = "William Butcher"
+print(person1.name)
+
+print(person1.age)
+person1.age = 83
+print(person1.age)
+
+# Deleting Properties -- DANGER WILL ROBINSON, DANGER
+# THIS IS DOES NOT 'RESET' A PROPERTY, IT DELETES IT COMPLETELY.
+print(person1.name)
+del person1.name
+#print(person1.name)
+print(person1.age)
+del person1.age
+#print(person1.age)
+
+# Deleting Objects -- Delete them if you're finished with them.
+del person1
+
+# Adding Properties to Objects -- USE CAREFULLY
+person2.weakness = "Kryptonite"
 print(person2)
-
-if person1.weight > person2.weight:
-    print(f"{person1.name} weighs more than {person2.name}.")
-elif person1.weight == person2.weight:
-    print("Each person weighs the same.\n")
-else:
-    print(f"{person2.name} weighs more than {person1.name}.")
-
-if person1.age > person2.age:
-    print(f"{person1.name} is older than {person2.name}.")
-elif person1.age == person2.age:
-    print(f"{person1.name} is the same age as {person2.name}")
-else:
-    print(f"{person2.name} is older than {person1.name}")
-
-person1.classfunction()
+print(person2.weakness)
