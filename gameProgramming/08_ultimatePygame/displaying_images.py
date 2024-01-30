3b31aa8f4f86746b452815318b1c9c412197d63c
NoahM24 <142345250+NoahM24@users.noreply.github.com>
2024-01-30 14:31:36 -0500

    yes
    yes

--- gameProgramming/08_ultimatePygame/displaying_images.py
+++ gameProgramming/08_ultimatePygame/displaying_images.py
@@ -1,16 +1,18 @@
 # Displaying Images, Noah Mulder, v1
+#(regular) surface: Essentially a single image (something imported, rendered text or a plain color). Needs to be put on display surface to be visible. Flexible amount and only displayed when connected to the display surface.
+# Display Surface: The window that the player sees when he starts the game. The Game Window. Anyhting displayed goes on here. Must be unique and is always visible
+# Origin, (0, 0), is in the top left corner of the display surface
 
 import pygame
 from sys import exit
 
 pygame.init()
-screen = pygame.display.set_mode((800, 400)) # Display Surface: The window that the player sees when he starts the game. The Game Window. Anyhting displayed goes on here. Must be unique and is always visible
+screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption("Runner")
 clock = pygame.time.Clock()
 
-test_surface = pygame.Surface((100,200)) #(regular) surface: Essentially a single image (something imported, rendered text or a plain color). Needs to be put on display surface to be visible. Flexible amount and only displayed when connected to the display surface.
-test_surface.fill('Red')
-
+sky_surface = pygame.image.load('img/ultimate_pygame/sky.jfif')
+ground_surface = pygame.image.load('img/ultimate_pygame/ground.jfif')
 
 while True:
     for event in pygame.event.get():
@@ -19,7 +21,8 @@ while True:
             exit()
     # draw all our elements
     # update everything
-    screen.blit(test_surface, (0, 0)) # Origin, (0, 0), is in the top left corner of the display surface
+    screen.blit(ground_surface(0, 100))
+    screen.blit(sky_surface, (0, 0))
     pygame.display.update()
     clock.tick(60)
 
