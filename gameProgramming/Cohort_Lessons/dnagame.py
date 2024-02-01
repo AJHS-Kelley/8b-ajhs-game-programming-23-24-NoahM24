b22312eaf6379f7228278d8c72227964a2285503
NoahM24 <142345250+NoahM24@users.noreply.github.com>
2024-02-01 14:16:19 -0500

    updating
    teh slow way

--- gameProgramming/Cohort_Lessons/dnagame.py
+++ gameProgramming/Cohort_Lessons/dnagame.py
@@ -22,3 +22,18 @@ def gen_dna() -> str:
         bases_generated += 1
         return dna_sequence
 dna = gen_dna()
+
+def do_transcription(dna_sequence: str) -> tuple:
+    print(f"The DNA sequence is {dna_sequence}.\n")
+    print("You will now generate the RNA sequence that would match.\n")
+    print("Please remember, in the RNA sequence U pairs with A from the DNA sequence.\n")
+    rna_start = time.time() # time.time() returns the number of seconds since 00:00:00 UTC Jan. 01, 1970
+    rna_sequence = input("Please enter the matching RNA sequence. Leave no spaces! Then press enter.\n").upper()
+    if rna_sequence not in 'AGCU':
+        print("That is not in the RNA sequence. Try again.\n")
+    rna_stop = time.time()
+    rna_time = rna_stop - rna_start
+    return (rna_sequence, rna_time)
+# Tuples are ORDERED -- you can reference elements with the index
+# Tuples are UNCHANGEABLE -- you cannot add, modify, or delete after creating
+# Tuples CAN have duplicates values.
