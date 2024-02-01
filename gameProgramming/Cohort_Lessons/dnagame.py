e9330288df5687a606a4ef62010c319fbbaa6b4c
NoahM24 <142345250+NoahM24@users.noreply.github.com>
2024-02-01 14:23:46 -0500

    updating...
    some crap

--- gameProgramming/Cohort_Lessons/dnagame.py
+++ gameProgramming/Cohort_Lessons/dnagame.py
@@ -37,3 +37,22 @@ def do_transcription(dna_sequence: str) -> tuple:
 # Tuples are ORDERED -- you can reference elements with the index
 # Tuples are UNCHANGEABLE -- you cannot add, modify, or delete after creating
 # Tuples CAN have duplicates values.
+
+def verify_sequence(dna_sequence: str, rna_sequence : str) -> bool:
+    is_match = False
+    if len(dna_sequence) != len(rna_sequence):
+        print("The amount of bases don't match.\n")
+        return is_match
+    for dna_base, rna_base in zip(dna_sequence, rna_sequence):
+        if dna_base == "A" and rna_base == "U":
+            is_match = True
+        elif dna_base == "C" and rna_base == "G":
+            is_match = True
+        elif dna_base == "G" and rna_base == "C":
+            is_match = True
+        elif dna_base == "T" and rna_base == "A":
+            is_match = True
+        else:
+            print("Not sure if bases match correctly. That's kinda cringe.\n")
+    return is_match
+
