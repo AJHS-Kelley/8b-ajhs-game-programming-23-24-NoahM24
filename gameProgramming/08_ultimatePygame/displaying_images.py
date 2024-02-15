5bc14d1059ab70ca8eb1aedc8aeeab9ca45777e3
NoahM24 <142345250+NoahM24@users.noreply.github.com>
2024-02-15 14:40:34 -0500

    e
    e

--- gameProgramming/08_ultimatePygame/displaying_images.py
+++ gameProgramming/08_ultimatePygame/displaying_images.py
@@ -13,6 +13,7 @@ def display_score():
     score_surf = test_font.render(f'Score: {current_time}', False, (64, 64, 64))
     score_rect = score_surf.get_rect(center = (400, 50))
     screen.blit(score_surf, score_rect)
+    return current_time
 
 
 pygame.init()
@@ -22,6 +23,7 @@ clock = pygame.time.Clock()
 test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
 game_active = True
 start_time = 0
+score = 0
 
 sky_surface = pygame.image.load('img/ultimate_pygame/Sky.png').convert()
 ground_surface = pygame.image.load('img/ultimate_pygame/ground.png').convert()
@@ -38,7 +40,13 @@ player_rect = player_surf.get_rect(topleft = (80, 210))
 player_gravity = 0
 player_stand = pygame.image.load('img/ultimate_pygame/homer_nipp.png').convert()
 player_stand_scaled = pygame.transform.scale(player_stand, (600, 300))
-player_stand_rect = player_stand.get_rect(center = (400, 200))
+player_stand_rect = player_stand.get_rect(center = (400, 190))
+
+game_name = test_font.render('Death Trap for the User', False, (111, 196, 169))
+game_name_rect = game_name.get_rect(center = (400, 40))
+
+game_message = test_font.render('Click screen to run', False, (111, 196, 169))
+game_message_rect = game_message.get_rect(center = (400, 380))
 
 while True:
     for event in pygame.event.get():
@@ -65,7 +73,7 @@ while True:
         #SKY    
         screen.blit(sky_surface, (0, 0))
         #SNAIL
-        snail_rect.left -= 6
+        snail_rect.left -= 10
         if snail_rect.right <= 0: snail_rect.left = 800
         screen.blit(snail_surface, snail_rect)
         
@@ -80,7 +88,7 @@ while True:
         # pygame.draw.rect(screen, 'White', score_rect)
         # pygame.draw.rect(screen, 'White', score_rect, 10)
         # screen.blit(score_surf, score_rect)
-        display_score()
+        score = display_score()
 
         #COLLISION
         if snail_rect.colliderect(player_rect):
@@ -88,6 +96,14 @@ while True:
     else:
         screen.fill((94, 129, 162))
         screen.blit(player_stand_scaled, player_stand_rect)
+
+        score_message = test_font.render(f'Your Score: {score}', False, (111, 196, 169))
+        score_message_rect = score_message.get_rect(center = (400, 380))
+        screen.blit(game_name, game_name_rect)
+
+        if score == 0: screen.blit(game_message, game_message_rect)
+        else: screen.blit(score_message, score_message_rect)
+
 
     pygame.display.update()
     clock.tick(60)
