550b10145dd93832232f9eab19e0d3a01f7e0769
NoahM24 <142345250+NoahM24@users.noreply.github.com>
2024-03-27 14:14:57 -0400

    e
    e

--- gameProgramming/09_final_project/final_project.py
+++ gameProgramming/09_final_project/final_project.py
@@ -1,1 +1,15 @@
 # Final Project, Noah Mulder, v0
+import pygame
+from sys import exit
+import random
+pygame.init()
+screen = pygame.display.set_mode((800, 400))
+pygame.display.set_caption("Death Trap for the User")
+clock = pygame.time.Clock()
+test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
