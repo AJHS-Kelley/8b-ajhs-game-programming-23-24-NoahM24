5604e46b1cf993a0f7e9718899aeb9d12e2d4a42
NoahM24 <142345250+NoahM24@users.noreply.github.com>
2024-04-03 14:03:38 -0400

    e
    e

---
+++ gameProgramming/11_slice_dice/slice_dice.py
@@ -0,0 +1,24 @@
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+my_string = "This is my string which consists of letters and words. L"
+
+print(alphabet[0])
+print(alphabet[-1])
+
+# Slicing
+print(alphabet[0:4]) # Slice UP TO second index
+# alphabet[start:stop]
+
+# Slice from the Start
+print(alphabet[:7])
+
+# Slice to the End
+print(alphabet[12:])
+
+# Slice the Whole %#$#! Thing
+print(alphabet[:])
+
+# Negative Index Slicing
+print(alphabet[-4:-1])
+
+# Messing Around
+print(my_string[24:])
