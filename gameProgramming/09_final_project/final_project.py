37df8b473a87ec5b3b8a76940e1860026cfd8690
NoahM24 <142345250+NoahM24@users.noreply.github.com>
2024-04-15 13:27:29 -0400

    e
    e

--- gameProgramming/09_final_project/final_project.py
+++ gameProgramming/09_final_project/final_project.py
@@ -15,6 +15,7 @@ screen = pygame.display.set_mode((x, y))
 pygame.init()
 clock = pygame.time.Clock()
 game_active = True
+test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
 
 starting_screen = pygame.image.load('img/poker/yahoo.png').convert()
 
@@ -25,6 +26,12 @@ if difficulty == 1:
 else:
     pygame.display.set_caption('G.Y.L.A -- CRACKED')
 
+game_name = test_font.render('G.Y.L.A', False, (0, 0, 0))
+game_name_rect = game_name.get_rect(center = (400, 40))
+
+game_message = test_font.render('Start', False, (0, 0, 0))
+game_message_rect = game_message.get_rect(center = (400, 380))
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
@@ -34,3 +41,6 @@ while True:
     
     if game_active:
         screen.blit(starting_screen, (0,0))
+
+    pygame.display.update()
+    clock.tick(60)
