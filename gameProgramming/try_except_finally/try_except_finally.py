759563dffd6bc06cd0f25c9c0def1016e7c443c9
NoahM24 <142345250+NoahM24@users.noreply.github.com>
2024-04-23 13:47:02 -0400

    e
    e

---
+++ gameProgramming/try_except_finally/try_except_finally.py
@@ -0,0 +1,14 @@
+# This is a method for testing code and preventing crashes.
+# try -- except -- else -- finally
+
+try: # The code in this block is ALWAYS executed
+    my_variable = 1
+    print(my_variabl)
+except NameError: # This code will run if there is an error (exception) raised.
+    print("There is an incorrect variable in your code. Fix it dummy.")
+except:
+    print("Soemthing else has gone worng.")
+else: # This code runs when there are NO ERRORS
+    print("Good job. Your code worked correctly for once. =)\n")
+finally: # THIS CODE ALWAYS RUNS, IT'S LIKE THE TERMINATOR
+    print("Get to the choppa!")
